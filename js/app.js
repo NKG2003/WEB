ed353f0c7987b4583bb319841cd4651ec3d1d490
NKG <kaliollanurbek14@gmail.com>
2023-11-06 16:38:48 +0600

    9 contact

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const homeTexts = {
         "kz": "Компания туралы ",
     }, 
     "history": {
-        "ru": "объем памяти",
+        "ru": "История",
         "en": "History",
         "kz": "Тарих",
     }, 
@@ -200,3 +200,21 @@ function checkBrowserLang() {
 }
 
 console.log("navigator.language", checkBrowserLang());
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
